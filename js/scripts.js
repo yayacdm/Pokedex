1eb65f7b584f6b2e80e91c494e1bc65e69de0253
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-04-14 11:34:08 -0700

    reorganized js pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
 let pokemonList = [
-  {name: 'Bulbasaur', height: 2.04, type: ['grass', 'poison']},
-  {name: 'Charmander', height: 2, type: ['fire']},
-  {name: 'Squirtle', height: 1.08, type: ['water']},
-  {name: 'Caterpie', height: 1, type: ['bug']},
-  {name: 'Weedle', height: 1, type: ['bug', 'poison']},
-  {name: 'Pidgey', height: 1, type: ['normal', 'flying']},
+  {
+    name: 'Bulbasaur',
+    height: 2.04,
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Charmander',
+    height: 2,
+    type: ['fire']
+  },
+  {
+    name: 'Squirtle',
+    height: 1.08,
+    type: ['water']
+  },
+  {
+    name: 'Caterpie',
+    height: 1,
+    type: ['bug']
+  },
+  {
+    name: 'Weedle',
+    height: 1,
+    type: ['bug', 'poison']},
+  {
+    name: 'Pidgey',
+    height: 1,
+    type: ['normal', 'flying']
+  },
 ];
