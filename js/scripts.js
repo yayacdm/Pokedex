36ca41048ea808587665e7b234e2dc184f8206c8
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-04-13 21:34:03 -0700

    added pokemon list

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let pokemonList = [
+  {name: 'Bulbasaur', height: 2.04, type: ['grass', 'poison']},
+  {name: 'Charmander', height: 2, type: ['fire']},
+  {name: 'Squirtle', height: 1.08, type: ['water']},
+  {name: 'Caterpie', height: 1, type: ['bug']},
+  {name: 'Weedle', height: 1, type: ['bug', 'poison']},
+  {name: 'Pidgey', height: 1, type: ['normal', 'flying']},
+];
