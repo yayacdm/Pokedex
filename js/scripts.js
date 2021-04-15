3db414178ca5738745a39f1b3435942bb1278eb3
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-04-15 13:55:26 -0700

    cleaned up old code

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,3 @@
-/*
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 2.04,
-    type: ['Grass', ' Poison']
-  },
-  {
-    name: 'Ivysaur',
-    height: 3.03,
-    type: ['Grass', ' Poison']
-  },
-  {
-    name: 'Venusaur',
-    height: 6.07,
-    type: ['Grass', ' Poison']
-  },
-  {
-    name: 'Charmander',
-    height: 2,
-    type: ['Fire']
-  },
-  {
-    name: 'Charmeleon',
-    height: 3.07,
-    type: ['Fire']
-  },
-  {
-    name: 'Charizard',
-    height: 5.07,
-    type: ['Fire', ' Flying']
-  },
-  {
-    name: 'Squirtle',
-    height: 1.08,
-    type: ['Water']
-  },
-  {
-    name: 'Wartortle',
-    height: 3.03,
-    type: ['Water']
-  },
-  {
-    name: 'Blastoise',
-    height: 5.03,
-    type: ['Water']
-  },
-];
-*/
 /* ORIGINAL CODE LOOP
 for (let i=0; i < pokemonList.length; i++) {
   if (pokemonList[i].height >6) {
@@ -57,7 +8,7 @@ for (let i=0; i < pokemonList.length; i++) {
 }
 */
 
-//ADDING IIFE
+// ADDING IIFE
 var pokemonRepository = (function () {
   var pokemonList = [
       {
