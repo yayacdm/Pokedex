2e853cd598c01c525aca00450d3d7b91f828a9d9
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-04-15 12:32:36 -0700

    add IIFE around pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+/*
 let pokemonList = [
   {
     name: 'Bulbasaur',
@@ -45,7 +46,7 @@ let pokemonList = [
     type: ['Water']
   },
 ];
-
+*/
 /* ORIGINAL CODE LOOP
 for (let i=0; i < pokemonList.length; i++) {
   if (pokemonList[i].height >6) {
@@ -56,6 +57,71 @@ for (let i=0; i < pokemonList.length; i++) {
 }
 */
 
+//ADDING IIFE
+let pokemonRepository = (function () {
+  let pokemonList = [
+      {
+        name: 'Bulbasaur',
+        height: 2.04,
+        type: ['Grass', ' Poison']
+      },
+      {
+        name: 'Ivysaur',
+        height: 3.03,
+        type: ['Grass', ' Poison']
+      },
+      {
+        name: 'Venusaur',
+        height: 6.07,
+        type: ['Grass', ' Poison']
+      },
+      {
+        name: 'Charmander',
+        height: 2,
+        type: ['Fire']
+      },
+      {
+        name: 'Charmeleon',
+        height: 3.07,
+        type: ['Fire']
+      },
+      {
+        name: 'Charizard',
+        height: 5.07,
+        type: ['Fire', ' Flying']
+      },
+      {
+        name: 'Squirtle',
+        height: 1.08,
+        type: ['Water']
+      },
+      {
+        name: 'Wartortle',
+        height: 3.03,
+        type: ['Water']
+      },
+      {
+        name: 'Blastoise',
+        height: 5.03,
+        type: ['Water']
+      },
+    ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+//CALL FOR POKEMON LIST
 pokemonList.forEach(function(pokemon) {
   document.write(pokemon.name + '- Height: ' + pokemon.height + ', Type: ' + pokemon.type + '<br>');
 });
